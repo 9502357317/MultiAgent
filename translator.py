7107d52d53ad835d1d4c6678307fad1faf825498
Surya Prakash Challa <119730439+9502357317@users.noreply.github.com>
2025-03-14 22:09:27 +0530

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,18 +1,17 @@
-from translate import Translator
+from googletrans import Translator
+from gtts import gTTS
+import playsound
+import os
 
 class LanguageTranslator:
     def __init__(self):
-        self.translator = Translator(to_lang="es")  # Default target language
+        self.translator = Translator()
 
     def translate_text(self, text, dest_language):
-        self.translator = Translator(to_lang=dest_language)
-        translation = self.translator.translate(text)
-        return translation
+        translation = self.translator.translate(text, dest=dest_language)
+        return translation.text
 
     def speak_text(self, text, language):
-        from gtts import gTTS
-        import playsound
-        import os
         tts = gTTS(text=text, lang=language)
         tts.save("output.mp3")
         playsound.playsound("output.mp3")
